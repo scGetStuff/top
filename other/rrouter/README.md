53d9bdbb578a82164c42305678e6b3304800516a
Scott <67354101+scGetStuff@users.noreply.github.com>
2024-11-19 13:23:43 -0500

    create React Router project

---
+++ other/rrouter/README.md
@@ -0,0 +1,16 @@
+# React Router
+
+https://www.theodinproject.com/lessons/node-path-react-new-react-router
+
+# setup
+
+```sh
+# run in parent directory
+npm create vite@latest rroute -- --template react
+cd rroute
+npm install
+npm run dev
+
+# clean out template CSS and jsx
+
+```
---
+++ other/rrouter/src/main.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
